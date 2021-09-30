d14a05d431566f48104ac9e69a46069f31c41d96
YongChan Cho <h3236516@gmail.com>
2021-09-30 22:34:20 +0900

    feat(exceptions): add LockfileNotFound

--- lockfilepy/exceptions.py
+++ lockfilepy/exceptions.py
@@ -1,3 +1,8 @@
 class LolClientNotFound(Exception):
     def __init__(self) -> None:
         super().__init__("League of legends client was not found.")
+
+
+class LockfileNotFound(Exception):
+    def __init__(self) -> None:
+        super().__init__("Lockfile was not found.")
